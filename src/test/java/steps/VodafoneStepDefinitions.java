d1749c00cb9e869ada4adb2b09c4d5698183625f
YahyaQandel <yahya.qandel@gmail.com>
2022-11-25 17:34:51 +0200

    handle vodafone cookie alert box popup

--- src/test/java/steps/VodafoneStepDefinitions.java
+++ src/test/java/steps/VodafoneStepDefinitions.java
@@ -26,24 +26,26 @@ public class VodafoneStepDefinitions {
     @Before
     public void setup() {
         WebDriverManager.chromedriver().setup();
-        ChromeOptions chromeOptions = new ChromeOptions().setHeadless(true);
+        ChromeOptions chromeOptions = new ChromeOptions().setHeadless(true);;
         this.driver = new ChromeDriver(chromeOptions);
     }
 
     @Given("I go to vodafone website")
     public void iGoToVodafoneWebsite() {
         driver.get(VODAFONE_WEBSITE_URL);
+        closeAllowCookieAlertBoxIfExists();
     }
 
     @When("I click on the search icon")
     public void iClickOnSearchIcon() {
-        WebElement searchIcon = waitForElementsToBeVisible(By.id("searchIcon"));
+
+        WebElement searchIcon = waitForElementToBeVisible(By.id("searchIcon"));
         searchIcon.click();
     }
 
     @And("I type in search field {string}")
     public void iTypeInSearchField(String textToSearch) {
-        WebElement searchField = waitForElementsToBeVisible(By.id("search"));
+        WebElement searchField = waitForElementToBeVisible(By.id("search"));
         searchField.sendKeys(textToSearch);
         searchField.sendKeys(Keys.ENTER);
     }
@@ -54,14 +56,21 @@ public class VodafoneStepDefinitions {
          List <WebElement> searchResultsElements = wait.until(
                 ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("result"))
         );
+//       return specifically the first element each time
          WebElement firstSearchResult = searchResultsElements.get(0);
-        Assert.assertEquals(searchResultText,firstSearchResult.getText());
+         Assert.assertEquals(searchResultText,firstSearchResult.getText());
     }
 
-    WebElement waitForElementsToBeVisible(By selector) {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+    WebElement waitForElementToBeVisible(By selector) {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
         return wait.until(
                 ExpectedConditions.visibilityOfElementLocated(selector));
+    }
+    void closeAllowCookieAlertBoxIfExists(){
+        List<WebElement> allowCookieAlertBoxCloseIconList = driver.findElements(By.id("onetrust-close-btn-container"));
+        if (allowCookieAlertBoxCloseIconList.size() > 0 && allowCookieAlertBoxCloseIconList.get(0).isDisplayed()){
+            allowCookieAlertBoxCloseIconList.get(0).findElement(By.tagName("button")).click();
+        }
     }
     @After
     public void teardown(){
